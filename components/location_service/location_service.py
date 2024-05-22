545e65dc07f4a23a3859da3cb18918553c935b44
Shakil Hossain <sh9743441@gmail.com>
2024-05-22 19:35:28 +0600

    fix class name, update comment.

--- components/location_service/location_service.py
+++ components/location_service/location_service.py
@@ -2,12 +2,11 @@
 from django_components import component
 
 @component.register("location_service")
-class Calendar(component.Component):
+class LocationService(component.Component):
     # Templates inside `[your apps]/components` dir and `[project root]/components` dir will be automatically found. To customize which template to use based on context
     # you can override def get_template_name() instead of specifying the below variable.
     template_name = "location_service/template.html"
 
-    # This component takes one parameter, a service_info string to show in the template
     def get_context_data(self, service_info, name , icon):
         return {
             "service_info": service_info,

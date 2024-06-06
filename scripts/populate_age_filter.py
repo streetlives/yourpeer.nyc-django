ac52b9c7afbf5c5efc0e191631d5e056491d3cbb
Jacob Beard <jake@minnow.io>
2024-06-05 21:39:50 -0400

    Ingest age filter CSV is working well.

--- scripts/populate_age_filter.py
+++ scripts/populate_age_filter.py
@@ -29,7 +29,8 @@ age_parameter_id = cur.fetchone()[0]
 cur.execute("delete from eligibility where parameter_id = %s", (age_parameter_id,))
 conn.commit()
 
-with open('20240508 YourPeer Age and Population Served sheet - services.csv') as f:
+
+with open('20240508 YourPeer Age and Population Served sheet - 20240508.csv') as f:
     rows = [
         {
             'should_process': bool(row['eligible_values']['ageMax'] or \
@@ -45,12 +46,7 @@ with open('20240508 YourPeer Age and Population Served sheet - services.csv') as
                     'ageMin': None if row['Age min'] == '' else int(row['Age min']),
                     'allAges': bool(row['All ages']),
                 },
-                'location_id': row['location_id'],
-                'service_name': row['service_name'],
-                'gogetta_url': row['GoGetta URL'],
-                'organization_id': row['id'],
-                'organization_name': row['Org name'],
-                'location_name': row['location_name'],
+                'gogetta_url': row['gogetta_link service'],
                 'row_num': row_num + 1,
             }
             for row_num, row in enumerate(csv.DictReader(f))
@@ -60,22 +56,16 @@ with open('20240508 YourPeer Age and Population Served sheet - services.csv') as
 print('number of rows', len(rows))
 #pprint(rows)
 
-def validate_location_id(location_id):
-    uuid_location_id = uuid.UUID(location_id)
-    # verify that location exists
-    cur.execute('select count(1) from locations where id = %s', (location_id, ))
-    assert cur.fetchone()[0] == 1
-
-def validate_organization_id(organization_id):
-    uuid_organization_id = uuid.UUID(organization_id)
-    # verify that organization exists
-    cur.execute('select count(1) from organizations where id = %s', (organization_id, ))
+def validate_service_id(service_id):
+    uuid_service_id = uuid.UUID(service_id)
+    # verify that service exists
+    cur.execute('select count(1) from services where id = %s', (service_id, ))
     assert cur.fetchone()[0] == 1
 
 with open('out.csv', 'w') as f:
 
     writer = csv.DictWriter(f, 
-        fieldnames=['status', 'derived_location_id', 'derived_service_id'] + 
+        fieldnames=['status'] + 
             list(rows[0].keys()))
     writer.writeheader()
 
@@ -88,76 +78,17 @@ with open('out.csv', 'w') as f:
             continue
 
         # do a quick sanity check
-        location_id = row['location_id']
         eligible_values = row['eligible_values']
 
-        errors = []
-
-        # validate the uuid (there are some malformed ones)
-        try: 
-            validate_location_id(location_id)
+        try:
+            service_id = urlparse(row['gogetta_url']).path.split('/')[-1]
+            validate_service_id(service_id)
         except:
-            errors.append(traceback.format_exc())
-            print(traceback.format_exc())
-            # if that doesn't work, try to look up the location by parsing GoGetta URL
-            try:
-                location_id = urlparse(row['gogetta_url']).path.split('/')[-1]
-                validate_location_id(location_id)
-            except:
-                errors.append(traceback.format_exc())
-                print(traceback.format_exc())
-                try:
-                    # if that doesn't work, then try to look up the location by organization id and location name
-                    organization_id = row['organization_id']
-                    location_name = row['location_name']
-                    validate_organization_id(organization_id)
-                    cur.execute("""
-                        select id::varchar as location_id from locations
-                        where organization_id = %s and name = %s
-                    """, (organization_id, location_name,))
-                    location_id = cur.fetchone()[0]
-                except:
-                    errors.append(traceback.format_exc())
-                    print(traceback.format_exc())
-                    # if that doesn't work, then try to look up the location by organization name and location name
-                    try:
-                        organization_name = row['organization_name']
-                        cur.execute("""
-                            select l.id::varchar as location_id 
-                            from locations l inner join organizations o 
-                            on l.organization_id = o.id
-                            where o.name = %s and l.name = %s
-                        """, (organization_name, location_name,))
-                        location_id = cur.fetchone()[0]
-                    except:
-                        errors.append(traceback.format_exc())
-                        print(traceback.format_exc())
-                        writer.writerow({
-                            'status': f'ERROR: Unable to find a location_id for given row: {";".join(errors)}',
-                            'derived_location_id': None, 
-                            'derived_service_id': None,
-                            **row
-                        })
-                        continue
-
-        # now that we have a location_id, look up the service id from the location_id and service name
-        service_name = row['service_name']
-        cur.execute("""
-            select sal.service_id from service_at_locations sal
-            inner join services s on sal.service_id = s.id
-            where s.name = %s and sal.location_id = %s
-        """, (service_name, location_id,))
-        service_id = cur.fetchone()
-        if not service_id:
             writer.writerow({
-                'status': f'ERROR: Unable to find service id for location and service name: {service_name} {location_id}',
-            'derived_location_id': location_id, 
-            'derived_service_id': None,
+                'status': f'ERROR: Unable to find a service_id for given row: {traceback.format_exc()}',
                 **row
             })
             continue
-        service_id = service_id[0]
-
 
         # accumulate results. If there's already a row in the database, append another age filter to him
         cur.execute('''
@@ -169,7 +100,6 @@ with open('out.csv', 'w') as f:
         existing_row = cur.fetchone()
         if existing_row:
             existing_eligible_values = existing_row[0]
-            print(existing_eligible_values)
             assert type(existing_eligible_values) == list
             cur.execute('''
                 update eligibility
@@ -211,8 +141,6 @@ with open('out.csv', 'w') as f:
 
         writer.writerow({
             'status': 'SUCCESS',
-            'derived_location_id': location_id, 
-            'derived_service_id': service_id,
             **row
         })
 
